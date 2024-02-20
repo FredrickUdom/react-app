e38c6e31495a0fa4b716afbead6175c2dca95276
“fredrickUdom” <fredrickudom@gmail.com>
2024-02-20 19:09:16 +0100

    dynamic value in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
 function App() {
+  const greet = 'Welcome to your first coding in ReactJs framework'
   return (
     <div className="App">
       <div className="content">
-        
+        <h1>{greet}</h1>
       </div>
     </div>
   );
