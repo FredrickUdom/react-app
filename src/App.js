43216494ee99abb9dde2e9f5c434f4ad52736f0e
“fredrickUdom” <fredrickudom@gmail.com>
2024-02-21 11:18:30 +0100

    dynamic values with math.random

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Navbar from './Navbar'
 function App() {
   const greet = 'Welcome to your first coding in ReactJs framework'
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
         <h1>{greet}</h1>
         <p>{Math.random() * 10}</p>
---
+++ src/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return ( 
+        dic
+     );
+}
+ 
+export default Home;
