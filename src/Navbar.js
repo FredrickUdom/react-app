0f87b904fdf6af5a612d9fd1b809738ba5faed26
“fredrickUdom” <fredrickudom@gmail.com>
2024-02-21 17:07:15 +0100

    applying css styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,14 @@
 const Navbar = () => {
     return ( 
-        <nav className="Navbar">
+        <nav className="navbar">
             <h1>My blog</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">Create</a>
+                <a href="/create" style={{
+                    color: 'white',
+                    backgroundColor:"#f1356d",
+                    borderRadius: '8px'
+                }}>Create</a>
             </div>
         </nav>
      );
