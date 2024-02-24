711769c416b8c22918668910c1c1e37332cc8a39
“fredrickUdom” <fredrickudom@gmail.com>
2024-02-24 23:57:52 +0100

    map component

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,27 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [] = useState([
+        {
+            title: 'My new website',
+            body: 'this is my website',
+            id:1
+        },
+
+        {
+            title: 'Welcome party',
+            body: 'this is a welcome party',
+            id:2
+        },
+
+        {
+            title: 'Web dev top tool',
+            body: 'this is for your web dev tools',
+            id:3
+        },
+    ]);
     return ( 
-        <h2>this is our homepage</h2>
+        <h1>welcome to homepage</h1>
      );
 }
  
