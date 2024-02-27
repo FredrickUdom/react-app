3e58ecf59a6f2cfe1ca1cfdcf17d020376cb1858
“fredrickUdom” <fredrickudom@gmail.com>
2024-02-27 14:07:37 +0100

    fetching the fake db from db.json file

--- src/Home.js
+++ src/Home.js
@@ -1,35 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Bloglist from "./Bloglist";
 
 const Home = () => {
     // eslint-disable-next-line
-    const [blogs, setBlogs] = useState([
-        {
-            title: 'My new website',
-            body: 'this is my website',
-            author: 'dickson',
-            id:1
-        },
-
-        {
-            title: 'Welcome party',
-            body: 'this is a welcome party',
-            author: 'mark',
-            id:2
-        },
-
-        {
-            title: 'Web dev top tool',
-            body: 'this is for your web dev tools',
-            author: 'james',
-            id:3
-        },
-    ]);
+    const [blogs, setBlogs] = useState(null);
     //  deleting blog 
     const handleDelete = (id)=>{
         const deleteBlog = blogs.filter(blog => blog.id !== id)
         setBlogs(deleteBlog)
     }
+    // fetch from the fake db.json file
+    useEffect(()=>{
+        fetch('http://localhost:8000/blogs')
+    })
     return ( 
         <div className="home">
            <Bloglist blogging={blogs} title='All Blogs' handleDelete={handleDelete}/>
