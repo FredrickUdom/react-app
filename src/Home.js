5b7e3db56cf5d5e7a63d7a4de7e1dd4a0db09662
“fredrickUdom” <fredrickudom@gmail.com>
2024-02-27 14:40:06 +0100

    conditional loading message

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,11 @@ import Bloglist from "./Bloglist";
 const Home = () => {
     // eslint-disable-next-line
     const [blogs, setBlogs] = useState(null);
-    //  deleting blog 
-   
+    
+    const [isPending, setIsPending]= useState(true)
     // fetch from the fake db.json file
     useEffect(()=>{
+      setTimeout(()=>{
         fetch('http://localhost:8000/blogs')
         .then((res)=>{
           return  res.json()
@@ -15,12 +16,16 @@ const Home = () => {
         .then((data)=>{
             // console.log(data)
             setBlogs(data)
+            setIsPending(false)
         })
+      }, 1000)
     })
     return ( 
         <div className="home">
+             {isPending && <div>Loading...</div>}
            {blogs && <Bloglist blogging={blogs} title='All Blogs'/>}
 
+           
            {/* filtering blog by name */}
 
            {/* <Bloglist blogging={blogs.filter((blog)=>blog.author === 'dickson')} title="this is dickson's blog"/> */}
