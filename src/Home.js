53ae81e67a8bda171bbdeb8d062e3285865ca065
“fredrickUdom” <fredrickudom@gmail.com>
2024-02-28 12:22:53 +0100

    making a custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,10 @@
-import { useEffect, useState } from "react";
+
 import Bloglist from "./Bloglist";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    // eslint-disable-next-line
-    const [blogs, setBlogs] = useState(null);
+   const {datas:blogs, isPending, error}=useFetch('http://localhost:8000/blogs')
     
-    const [isPending, setIsPending]= useState(true);
-
-    const [error, setError] = useState(null);
-    // fetch from the fake db.json file
-    useEffect(()=>{
-      setTimeout(()=>{
-        fetch('http://localhost:8000/blogs')
-        .then((res)=>{
-            if(!res.ok){
-                throw Error('Could not fetch data for that resource')
-            }
-          return  res.json()
-        })
-        .then((data)=>{
-            // console.log(data)
-            setBlogs(data)
-            setIsPending(false)
-            setError(null)
-        })
-        .catch(err =>{
-            const errMes = 'I can not load datas'
-            setError( `${errMes}`)
-            setIsPending(false)
-        })
-      }, 1000)
-    })
     return ( 
         <div className="home">
           {error && <div>{error}</div>}
---
+++ src/useFetch.js
@@ -0,0 +1,42 @@
+import {useState, useEffect} from 'react';
+const useFetch=(url)=>{
+
+
+     // eslint-disable-next-line
+     const [datas, setDatas] = useState(null);
+    
+     const [isPending, setIsPending]= useState(true);
+ 
+     const [error, setError] = useState(null);
+
+    // fetch from the fake db.json file
+    useEffect(()=>{
+        setTimeout(()=>{
+          fetch(url)
+          .then((res)=>{
+              if(!res.ok){
+                  throw Error('Could not fetch data for that resource')
+              }
+            return  res.json()
+          })
+          .then((data)=>{
+              // console.log(data)
+              setDatas(data)
+              setIsPending(false)
+              setError(null)
+          })
+          .catch(err =>{
+              const errMes = 'I can not load datas'
+              setError( `${errMes}`)
+              setIsPending(false)
+          })
+        }, 1000);
+      }, [url]);
+
+      return{
+        datas, 
+        isPending,
+        error
+      }
+}
+export default useFetch;
